571abcd05e4e9b2e0a3c93be52f7c261a1ef4571
ValdecirArruda1980 <valdecirrogerio@gmail.com>
2026-01-08 19:34:41 -0300

    Projeto: Calculadora de Preco Total

---
+++ Calculadora de Preço Total.py
@@ -0,0 +1,6 @@
+nome_produto = "Cadeira Infantil"
+preco_unit = 12.40
+quantidade = 3
+preco_total = quantidade * preco_unit
+print(f"Produto: {nome_produto}")
+print(f"Preço Total: R$ {preco_total:.2f}")
